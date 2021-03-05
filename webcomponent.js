7d82bd67084e51666c40a88d204832d0a413e9f4
messifressi <67953592+messifressi@users.noreply.github.com>
2021-03-05 14:45:52 +0100

    Update webcomponent.js

--- webcomponent.js
+++ webcomponent.js
@@ -1,98 +1,6 @@
 (function()  {
     let tmpl = document.createElement('template');
     tmpl.innerHTML = `
-	<svg
-   xmlns:dc="http://purl.org/dc/elements/1.1/"
-   xmlns:cc="http://creativecommons.org/ns#"
-   xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
-   xmlns:svg="http://www.w3.org/2000/svg"
-   xmlns="http://www.w3.org/2000/svg"
-   xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
-   xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
-   width="280.0px"
-   height="180.0px"
-   viewBox="0 0 280.0 180.0"
-   version="1.1"
-   id="SVGRoot">
-  <g
-     id="layer1"
-     inkscape:groupmode="layer"
-     inkscape:label="Ebene 1">
-    <text
-       id="textHeader"
-       y="13%"
-       x="8%"
-       style="font-style:normal;font-weight:normal;font-size:18pt;line-height:1.25;font-family:sans-serif;fill:#000000;fill-opacity:1;stroke:none;stroke-width:1.02012"
-       xml:space="preserve"><tspan
-         style="stroke-width:1.0"
-         y="23%"
-         x="5%"
-         id="ksText"
-         sodipodi:role="line">Kontrollspur N</tspan></text>
-    <text
-       id="textOpen"
-       y="17%"
-       x="8%"
-       style="font-style:normal;font-weight:normal;font-size:16pt;line-height:1.25;font-family:sans-serif;fill:#00b300;fill-opacity:1;stroke:none;stroke-width:1.02562"
-       xml:space="preserve"><tspan
-         style="stroke-width:1.0"
-         y="40%"
-         x="5%"
-         id="ksOpen"
-         sodipodi:role="line">status</tspan></text>
-    <rect
-       y="1%"
-       x="1%"
-       height="98%"
-       width="98%"
-       id="rect126"
-       style="fill:none;stroke:#000000;stroke-width:1.0;stroke-opacity:1;opacity:1" />
-    <text
-       id="text1305"
-       y="94.285713"
-       x="31.428572"
-       style="font-style:normal;font-weight:normal;font-size:10pt;line-height:1.25;font-family:sans-serif;fill:#000000;fill-opacity:1;stroke:none"
-       xml:space="preserve"><tspan
-         style="font-size:12pt"
-         y="65%"
-         x="5%"
-         id="paxKumTxt"
-         sodipodi:role="line">PAX kumuliert</tspan></text>
-    <text
-       id="text1309"
-       style="font-size:16px;line-height:1.25;font-family:sans-serif"
-       xml:space="preserve"><tspan
-         style="font-size:24pt"
-         y="85%"
-         x="5%"
-         id="paxKumVal"
-         sodipodi:role="line">0000</tspan></text>	
-    <text
-       id="text1330"
-       style="font-size:16px;line-height:1.25;font-family:sans-serif"
-       xml:space="preserve"><tspan
-         style="font-size:12pt"
-         y="65%"
-         x="50%"
-         id="tagesauslastung"
-         sodipodi:role="line">Tagesauslastung</tspan></text>
-    <rect
-       y="75%"
-       x="50%"
-       height="14.125"
-       width="120"
-       id="rect1332"
-       style="opacity:1;fill:#808080;fill-opacity:1;stroke:none;stroke-opacity:1" />
-	<path 
-	style="fill:none;stroke:#000000;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
-	d="m 140,127
-           v 30" />
-	<path 
-	style="fill:none;stroke:#000000;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
-	d="m 260,127
-           v 30" />
-  </g>
-</svg>
     `;
 	
 	
@@ -105,6 +13,9 @@
 			this._shadowRoot = this.attachShadow({mode: "open"});
             this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
             this._firstConnection = false;
+			this._tagContainer;
+            this._tagType = "h1";
+            this._tileHeaderText = "Hello World";
 		}
 
         //Fired when the widget is added to the html DOM of the page
@@ -158,6 +69,17 @@
 			}
 		}
 		
+		setTileValues(newValue1, newValue2){
+		}
+		
+		get tileHeaderText(){
+			return this._tileHeaderText;
+		}
+		
+		set tileHeaderText(value){
+			this._tileHeaderText = value;
+		}
+		
 		
 		setSquare(newValues){
 			var square4 = this._shadowRoot.querySelector("#square1");
@@ -167,6 +89,15 @@
 		}
 
         redraw(){
+			 if (this._tagContainer){
+                this._tagContainer.parentNode.removeChild(this._tagContainer);
+            }
+
+            var shadow = window.getSelection(this._shadowRoot);
+            this._tagContainer = document.createElement(this._tagType);
+            var theText = document.createTextNode(this._tileHeaderText);    
+            this._tagContainer.appendChild(theText); 
+            this._shadowRoot.appendChild(this._tagContainer);
 			
         }
     });
