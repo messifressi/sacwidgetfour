739e7ef6c6af62ba5766880a739c8f675678b360
messifressi <67953592+messifressi@users.noreply.github.com>
2021-03-12 14:27:42 +0100

    Update webcomponent.js

--- webcomponent.js
+++ webcomponent.js
@@ -1,6 +1,145 @@
 (function()  {
     let tmpl = document.createElement('template');
     tmpl.innerHTML = `
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<svg
+   xmlns:dc="http://purl.org/dc/elements/1.1/"
+   xmlns:cc="http://creativecommons.org/ns#"
+   xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+   xmlns:svg="http://www.w3.org/2000/svg"
+   xmlns="http://www.w3.org/2000/svg"
+   xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+   xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+   inkscape:version="1.0 (4035a4fb49, 2020-05-01)"
+   sodipodi:docname="ZeichnungWidget2.svg"
+   id="SVGRoot"
+   version="1.1"
+   viewBox="0 0 280.0 180.0"
+   height="180.0px"
+   width="280.0px">
+  <metadata
+     id="metadata24">
+    <rdf:RDF>
+      <cc:Work
+         rdf:about="">
+        <dc:format>image/svg+xml</dc:format>
+        <dc:type
+           rdf:resource="http://purl.org/dc/dcmitype/StillImage" />
+      </cc:Work>
+    </rdf:RDF>
+  </metadata>
+  <defs
+     id="defs22" />
+  <sodipodi:namedview
+     inkscape:current-layer="layer1"
+     inkscape:window-maximized="1"
+     inkscape:window-y="-8"
+     inkscape:window-x="-8"
+     inkscape:cy="104.36537"
+     inkscape:cx="86.356726"
+     inkscape:zoom="2.9556977"
+     showgrid="true"
+     id="namedview20"
+     inkscape:window-height="1017"
+     inkscape:window-width="1920"
+     inkscape:pageshadow="2"
+     inkscape:pageopacity="0"
+     guidetolerance="10"
+     gridtolerance="10"
+     objecttolerance="10"
+     borderopacity="1"
+     bordercolor="#666666"
+     pagecolor="#ffffff">
+    <inkscape:grid
+       id="grid849"
+       type="xygrid" />
+  </sodipodi:namedview>
+  <g
+     inkscape:label="Ebene 1"
+     inkscape:groupmode="layer"
+     id="layer1">
+    <text
+       xml:space="preserve"
+       style="font-style:normal;font-weight:normal;font-size:24px;line-height:1.25;font-family:sans-serif;fill:#000000;fill-opacity:1;stroke:none;stroke-width:1.02012"
+       x="19.693363"
+       y="32.87323"
+       id="textHeader"><tspan
+         sodipodi:role="line"
+         id="ksText"
+         x="19.693363"
+         y="32.87323"
+         style="stroke-width:1">Kontrollspur N</tspan></text>
+    <text
+       transform="scale(1.0149042,0.98531463)"
+       xml:space="preserve"
+       style="font-style:normal;font-weight:normal;font-size:21.2239px;line-height:1.25;font-family:sans-serif;fill:#00b300;fill-opacity:1;stroke:none;stroke-width:1.02036"
+       x="25.463541"
+       y="60.572983"
+       id="textOpen"><tspan
+         sodipodi:role="line"
+         id="ksOpen"
+         x="25.463541"
+         y="60.572983"
+         style="stroke-width:0.994873">status</tspan></text>
+    <rect
+       style="fill:none;stroke:#000000;stroke-width:1.0;stroke-opacity:1;opacity:1"
+       id="rect126"
+       width="98%"
+       height="98%"
+       x="1%"
+       y="1%" />
+    <text
+       transform="scale(1.0087215,0.99135386)"
+       xml:space="preserve"
+       style="font-style:normal;font-weight:normal;font-size:13.0174px;line-height:1.25;font-family:sans-serif;fill:#000000;fill-opacity:1;stroke:none;stroke-width:0.976308"
+       x="13.344829"
+       y="102.65318"
+       id="text1305"><tspan
+         sodipodi:role="line"
+         id="paxKumTxt"
+         x="13.344829"
+         y="102.65318"
+         style="font-size:15.6209px;stroke-width:0.976308">PAX kumuliert</tspan></text>
+    <text
+       y="153.73438"
+       x="12.859375"
+       xml:space="preserve"
+       style="font-size:16px;line-height:1.25;font-family:sans-serif"
+       id="text1309"><tspan
+         sodipodi:role="line"
+         id="paxKumVal"
+         x="12.859375"
+         y="153.73438"
+         style="font-size:32px">0000</tspan></text>
+    <text
+       y="102.15625"
+       x="139.5"
+       xml:space="preserve"
+       style="font-size:16px;line-height:1.25;font-family:sans-serif"
+       id="text1330"><tspan
+         sodipodi:role="line"
+         id="tagesauslastung"
+         x="139.5"
+         y="102.15625"
+         style="font-size:16px">Tagesauslastung</tspan></text>
+    <rect
+       style="opacity:1;fill:#808080;fill-opacity:1;stroke:none;stroke-opacity:1"
+       id="rect1332"
+       width="120"
+       height="14.125"
+       x="140"
+       y="138" />
+    <path
+       id="path14"
+       d="m 140,130 v 30"
+       style="fill:none;stroke:#000000;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1" />
+    <path
+       id="path16"
+       d="m 260,130 v 30"
+       style="fill:none;stroke:#000000;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1" />
+  </g>
+</svg>
+
     `;
 	
 	
